b8e6defb629da3d4c8ee0676a3b1f98902221640
quachnamluong <quachnamluong@gmail.com>
2024-03-12 21:06:36 +0700

    init project

---
+++ app/src/main/java/com/team18/recordapp/FragmentListRecord.java
@@ -0,0 +1,19 @@
+package com.team18.recordapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentListRecord extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_record, container, false);
+    }
+}
